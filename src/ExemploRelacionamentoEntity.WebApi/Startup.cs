100007cd401f4de8eac8e69bcfe2c37e9f0b85e8
Fernando Nogueira <fgeraldo11@gmail.com>
2022-01-11 11:41:39 -0300

    Injeção dos serviços

--- src/ExemploRelacionamentoEntity.WebApi/Startup.cs
+++ src/ExemploRelacionamentoEntity.WebApi/Startup.cs
@@ -1,15 +1,13 @@
+using ExemploRelacionamentoEntity.Data;
+using ExemploRelacionamentoEntity.Data.Context;
+using ExemploRelacionamentoEntity.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExemploRelacionamentoEntity.WebApi
 {
@@ -22,18 +20,23 @@ namespace ExemploRelacionamentoEntity.WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+
+            services.AddServices();
+            services.AddRepositories();
+
+            services.AddDbContext<ExemploContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("Connection")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExemploRelacionamentoEntity.WebApi", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
